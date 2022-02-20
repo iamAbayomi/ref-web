335450296b9f859d1fe330220135f34cce690f81
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-20 03:55:35 +0100

    style: style the header text

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Primary from '../components/ui/Button/Primary'
 
 const IndexPage = () => (
   <Layout>
-      <p>Refer & Earn</p>
+      <p className='header-text'>Refer & Earn</p>
       <p>Earn #100 airtime when you refer people 
         in Ibadan only</p>
       <p>Enter your phone number to receive airtime</p>
