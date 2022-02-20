f68119d90c663d9f9531231dc24bf464fddbbfcd
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-20 04:13:24 +0100

    style: added input field to take users phone no

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,10 @@ import Primary from '../components/ui/Button/Primary'
 const IndexPage = () => (
   <Layout>
       <p className='header-text'>Refer & Earn</p>
-      <p>Earn #100 airtime when you refer people 
+      <p className='info-text'>Earn #100 airtime when you refer people 
         in Ibadan only</p>
-      <p>Enter your phone number to receive airtime</p>
+      <p className='highlight-text'>Enter your phone number to receive airtime</p>
+      <input  type={"name"} placeholder='' />
       <Primary buttonTitle={'Share Referral Link'} nextPage='/accesscontact'/>
   </Layout>
 )
