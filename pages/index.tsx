2675c4188dd88e76a6ac3afb824a8753ee99114d
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-20 03:50:45 +0100

    refactor: added defualt homepage component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 import Layout from '../components/Layouts'
+import Primary from '../components/ui/Button/Primary'
 
 const IndexPage = () => (
   <Layout>
-    
+      <p>Refer & Earn</p>
+      <p>Earn #100 airtime when you refer people 
+        in Ibadan only</p>
+      <p>Enter your phone number to receive airtime</p>
+      <Primary buttonTitle={'Share Referral Link'} nextPage='/accesscontact'/>
   </Layout>
 )
 
